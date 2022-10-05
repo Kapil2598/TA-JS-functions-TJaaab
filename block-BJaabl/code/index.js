e691d3e1282519e1eff5cbc4f236b957d75c1864
Yashvardhan <kapilshakya2@gmail.com>
2022-10-05 21:55:00 +0530

    arguments and return

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -13,7 +13,9 @@ Example:
   getFullName("John", "Snow"); // "John Snow"
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
-
+function getFullName() {
+  
+}
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
 the sum of both numbers. If user passes any data other then numbers alert a message saying `Enter Valid Input`
@@ -23,7 +25,9 @@ addTwoNumbers(10, 22); // 32
 addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
+function addTwoNumbers() {
 
+}
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
 can only be one of these `add, sub, mul, div`. Based on the operator return the value. If the `numA` or `numB`
@@ -35,7 +39,10 @@ calc(10, 20, 'add'); // 30
 calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
+function calc()
+{
 
+}
 
 /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
@@ -45,7 +52,12 @@ isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
 
+function isLeapYear() {
 
+}
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
 */
+function getFactorial() {
+  
+}
